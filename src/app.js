afe5b68d7c1a52d5d64c5e15c532eaf7a540791e
Rancune <3519851+Rancune@users.noreply.github.com>
2022-07-13 11:37:41 +0200

    Nodemon Up on BackEnd

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
-
-
 const express = require('express')
 const app = express()
 const port = 3000
 
 
-
-
-
   app.get('/', (req, res) => {
     res.send('"JE SUIS UNE PUTAIN DE GENIE! "Le mechant dans Golden Eye.')
   })
+  app.post('/', (req, res) => {
+    res.send(`J'adore les petits pains d'épices!`)
+  })
 
   app.listen(port, () => {
-    console.log(`Example app listening on port sa mere ${port}`)
+    console.log(`Example app listening on port ${port}`)
   }
   )
-
-
-// Language: javascript
