4c97650f938557d32af6b2bbfe39cfec382373a8
Rancune <3519851+Rancune@users.noreply.github.com>
2022-07-13 10:42:29 +0200

    Nodemon init

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
+
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('"JE SUN PUTAIN DE GENIE! "Le mechant dans Golden Eye.')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+
+  app.get('/', (req, res) => {
+    res.send('"JE SUIS UNE PUTAIN DE GENIE! "Le mechant dans Golden Eye.')
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port sa mere ${port}`)
+  }
+  )
+
+
+// Language: javascript
