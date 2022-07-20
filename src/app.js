e3c1b3843f2efea1fae00aa45c0f051cc07c293c
Rancune <3519851+Rancune@users.noreply.github.com>
2022-07-20 13:19:43 +0200

    modif  on uri for test purpose

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const port = 3000
 
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
 const startApp = async () => {
--- src/controllers/connectionDatabase.js
+++ src/controllers/connectionDatabase.js
@@ -8,25 +8,24 @@ const { connect } = pkg;
 
 export const connectDatabase = async () => {
 
-    // connect(process.env.MONGO_DB_HOST, {
-    //     user: process.env.MONGO_DB_USER,
-    //     dbName: process.env.MONGO_DB_DATABASE,
-    //     pass: process.env.MONGO_DB_PASS,
-    //     maxPoolSize: 10, // Maintain up to 10 socket connections
-    //     // serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    //     // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    //   });
+  // connect(process.env.MONGO_DB_HOST, {
+  //   user: process.env.MONGO_DB_USER,
+  //   dbName: process.env.MONGO_DB_DATABASE,
+  //   pass: process.env.MONGO_DB_PASS,
+  //   authSource: process.env.MONGO_DB_AUTH_SOURCE,
+  //   authMechanism: process.env.MONGO_DB_AUTH_MECHANISM,
+  //   maxPoolSize: 10, // Maintain up to 10 socket connections
+  //   // serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  //   // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  // });
 
-      //
+ 
       
-    await connect('mongodb://root:root@localhost:27017/voyage?authSource=admin&authMechanism=SCRAM-SHA-1');// je sais foutre pas pourquoi ni comment le authSource fonctionne 
-    console.log('########################### connected to database ###########################');
-
-   
-
+  await connect('mongodb://root:root@localhost:27017/voyage?authSource=admin&authMechanism=DEFAULT');// je sais foutre pas pourquoi ni comment le authSource fonctionne 
+  console.log('########################### connected to database ###########################');
 
 }
 
 //connectDatabase().catch(err => console.log(err));
-// module.exports.Journey = mongoose.model('Journey', journeySchema);
+//module.exports.Journey = mongoose.model('Journey', journeySchema);
 
